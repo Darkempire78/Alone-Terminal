1bcd17fdc78548c48d4f22f844e200148dd0a6a4
Darkempire <50015928+Darkempire78@users.noreply.github.com>
2024-08-10 16:43:14 +0200

    test non concluant

--- ExcelExtractor.py
+++ ExcelExtractor.py
@@ -2,6 +2,8 @@ import pandas as pd
 import ast
 import json
 import codecs
+from openpyxl import load_workbook
+from openpyxl_image_loader import SheetImageLoader
 
 def isNaN(num):
     return num != num
@@ -12,10 +14,16 @@ def excelExtractor(mobileDevice=False):
     add_event_df = pd.read_excel('event_table.xlsx', sheet_name=1)
     loot_df = pd.read_excel('event_table.xlsx', sheet_name=2)
 
+    # For images
+    event_df_images = load_workbook('event_table.xlsx').worksheets[0]
+    event_df_image_loader = SheetImageLoader(event_df_images)
+
     event = {}
     
     # Iterate over rows in the event sheet
+    index = 0
     for _, row in event_df.iterrows():
+        index += 1
         # Ignore first line
         if row[0] == "zone":
             continue
@@ -26,37 +34,46 @@ def excelExtractor(mobileDevice=False):
             event[zone] = {"script": {}, "random": {}}
 
         name = row[2]
-        text = row[3]
+        image = row[3]
+
+        cell = event_df_images.cell(row=index, column=4).coordinate
+        if event_df_image_loader.image_in(cell):
+            image = event_df_image_loader.get(cell)
+            print(image)
+            image.show()
+
+
+        text = row[4]
         
         event[zone][type_][name] = {"text": text, "actions": {}}
         
         # Action 1
         event[zone][type_][name]["actions"]["action1"] = {
-            "description": row[4] if isNaN(row[4]) == False else "<empty>",
-            "next_message": row[5] if isNaN(row[5]) == False else "<empty>",
-            "health": int(row[6]),
-            "oxygen": int(row[7]),
-            "loot": row[8].split(", ") if pd.notna(row[8]) else [],
-            "add": ast.literal_eval(row[9]) if pd.notna(row[9]) else [],
-            "remove": ast.literal_eval(row[10]) if pd.notna(row[10]) else [],
-            "script": row[11].split(", ") if pd.notna(row[11]) else [],
-            "zone": row[12] if isNaN(row[12]) == False else None,
-            "require": row[13].split(", ") if pd.notna(row[13]) else []
+            "description": row[5] if isNaN(row[5]) == False else "<empty>",
+            "next_message": row[6] if isNaN(row[6]) == False else "<empty>",
+            "health": int(row[7]),
+            "oxygen": int(row[8]),
+            "loot": row[9].split(", ") if pd.notna(row[9]) else [],
+            "add": ast.literal_eval(row[10]) if pd.notna(row[10]) else [],
+            "remove": ast.literal_eval(row[11]) if pd.notna(row[11]) else [],
+            "script": row[12].split(", ") if pd.notna(row[12]) else [],
+            "zone": row[13] if isNaN(row[13]) == False else None,
+            "require": row[14].split(", ") if pd.notna(row[14]) else []
         }
 
         # Action 2 if it exists
-        if len(row) > 14 and pd.notna(row[14]):
+        if len(row) > 15 and pd.notna(row[14]):
             event[zone][type_][name]["actions"]["action2"] = {
-                "description": row[14] if isNaN(row[14]) == False else "<empty>",
-                "next_message": row[15] if isNaN(row[15]) == False else "<empty>",
-                "health": int(row[16]),
-                "oxygen": int(row[17]),
-                "loot": row[18].split(", ") if pd.notna(row[18]) else [],
-                "add": ast.literal_eval(row[19]) if pd.notna(row[19]) else [],
-                "remove": ast.literal_eval(row[20]) if pd.notna(row[20]) else [],
-                "script": row[21].split(", ") if pd.notna(row[21]) else [],
-                "zone": row[22] if isNaN(row[22]) == False else None,
-                "require": row[23].split(", ") if pd.notna(row[23]) else []
+                "description": row[15] if isNaN(row[15]) == False else "<empty>",
+                "next_message": row[16] if isNaN(row[16]) == False else "<empty>",
+                "health": int(row[17]),
+                "oxygen": int(row[18]),
+                "loot": row[19].split(", ") if pd.notna(row[19]) else [],
+                "add": ast.literal_eval(row[20]) if pd.notna(row[20]) else [],
+                "remove": ast.literal_eval(row[21]) if pd.notna(row[21]) else [],
+                "script": row[22].split(", ") if pd.notna(row[22]) else [],
+                "zone": row[23] if isNaN(row[23]) == False else None,
+                "require": row[24].split(", ") if pd.notna(row[24]) else []
             }
 
     if mobileDevice:
